11de626607929a907885ea57f41e36f0e16c2383
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-07-20 17:44:27 +0800

    Add additional path for assets in webpacker.yml and import app.css in app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import 'stylesheets/application'
 
 Rails.start()
 Turbolinks.start()
